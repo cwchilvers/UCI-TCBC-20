9cdb60be6b963247b8fb509a22afb52c83388a99
Chandler Chilvers <cwchilvers@gmail.com>
2023-08-24 09:53:03 -0700

    Started contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,6 +13,24 @@ function Contact () {
             </Helmet>
             <Header />
             <Title title='Contact' icon={icon} />
+            <form>
+                <div className="form-row">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" id="name" required />
+                </div>
+
+                <div className="form-row">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" id="email" required />
+                </div>
+
+                <div className="form-row">
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" id="message" required></textarea>
+                </div>
+
+                <button type="submit">Send</button>
+            </form>
             <Footer />
         </div>
     );
